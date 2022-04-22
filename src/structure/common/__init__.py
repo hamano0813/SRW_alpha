88bc80f52ce4e5845b60dbac1fb2ccfcc870ed93
HamanoTaiki <hamano0813@gmail.com>
2022-04-22 16:26:01 +0800

    #8 更新通用数据结构文本和数值解析

--- src/structure/common/__init__.py
+++ src/structure/common/__init__.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from .value import Value
+from .text import Text
---
+++ src/structure/common/text.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from struct import unpack_from, pack_into
+
+
+class Text:
+    def __init__(self, offset, length, aliases, additional=None, dummy=None):
+        self.offset: int = offset
+        self.length: int = length
+        self.aliases: str = aliases
+        self.additional: dict = additional
+        self.dummy: bytearray = dummy
+
+    def get(self, buffer: bytearray) -> str:
+        data = unpack_from(f'{self.length}s', buffer, self.offset)[0]
+        return self._decode_text(data)
+
+    def set(self, text: str, buffer: bytearray) -> str:
+        data = self._encode_text(text) + b'\00'
+        data += self.dummy[len(data):]
+        pack_into(f'{self.length}s', buffer, self.offset, data)
+        return self._decode_text(data)
+
+    def _decode_text(self, data: bytes) -> str:
+        temp = data.split(b'\00')[0].replace(b'\r', b'\n').decode(self.aliases)
+        if self.additional:
+            for old, new in self.additional.items():
+                temp = temp.replace(old, new)
+        return temp
+
+    def _encode_text(self, text: str) -> bytes:
+        temp = text
+        for old, new in reversed(self.additional.items()):
+            temp = temp.replace(new, old)
+        return temp.replace('ー', '-').encode(self.aliases).replace(b'\n', b'\r')
