b54adf6682d610dcd106dd9720cc56d38e22b10b
HamanoTaiki <hamano0813@gmail.com>
2022-04-29 23:44:49 +0800

    #50 更新新增AI解析模块

---
+++ src/structure/specific/aiunp_bin/aiunp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from structure.generic import Rom
+
+
+class AiunpBIN(Rom):
+    def __init__(self):
+        super(AiunpBIN, self).__init__()
+        self.structures = {
+
+        }
