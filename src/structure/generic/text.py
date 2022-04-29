3aee79f791e8090614115f64dbd911a921ec57e4
HamanoTaiki <hamano0813@gmail.com>
2022-04-29 21:48:36 +0800

    #44 更新文本解析类以去除替换BUG并修正文本写入逻辑

--- src/structure/generic/text.py
+++ src/structure/generic/text.py
@@ -5,28 +5,26 @@ from struct import unpack_from, pack_into
 
 
 class Text:
-    def __init__(self, offset: int, length: int, code: str, extra: dict = None, dummy: bytearray = None):
+    def __init__(self, offset: int, length: int, code: str, extra: dict = None):
         self.offset = offset
         self.length = length
         self.code = code
         self.extra = extra
-        self.dummy = dummy
 
     def parse(self, buffer: bytearray) -> str:
         data = unpack_from(f'{self.length}s', buffer, self.offset)[0].split(b'\00')[0]
         return self._decode_text(data)
 
     def build(self, text: str, buffer: bytearray) -> bytearray:
+        _buffer = bytearray(self.length)
         data = self._encode_text(text)[:self.length - 1] + b'\00'
         length = len(data)
-        if self.dummy:
-            data += self.dummy[length:]
-            length = self.length
-        pack_into(f'{length}s', buffer, self.offset, data)
+        pack_into(f'{length}s', _buffer, self.offset, data)
+        buffer[:] = _buffer
         return buffer
 
     def _decode_text(self, data: bytes) -> str:
-        temp = data.replace(b'\r', b'\n').replace(b'\x81@', b' ').decode(self.code)
+        temp = data.decode(self.code)
         if self.extra:
             for old, new in self.extra.items():
                 temp = temp.replace(old, new)
@@ -34,6 +32,7 @@ class Text:
 
     def _encode_text(self, text: str) -> bytearray:
         temp = text
-        for old, new in reversed(self.extra.items()):
-            temp = temp.replace(new, old)
-        return bytearray(temp.encode(self.code).replace(b'\n', b'\r').replace(b' ', b'\x81@'))
+        if self.extra:
+            for old, new in reversed(self.extra.items()):
+                temp = temp.replace(new, old)
+        return bytearray(temp.encode(self.code))
