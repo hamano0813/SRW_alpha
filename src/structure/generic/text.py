8d4abb8fc2cbef2d400c487b686f8288f44195ad
HamanoTaiki <hamano0813@gmail.com>
2022-05-01 00:39:48 +0800

    #56 更新完善TEXT解析功能

--- src/structure/generic/text.py
+++ src/structure/generic/text.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import copy
 from struct import unpack_from, pack_into
 
 
 class Text:
-    def __init__(self, offset: int, length: int, code: str, extra: dict = None):
+    def __init__(self, offset: int, length: int, code: str, extra: dict = None, dummy: bytearray = None):
         self.offset = offset
         self.length = length
         self.code = code
         self.extra = extra
+        self.dummy = dummy
 
     def parse(self, buffer: bytearray) -> str:
         data = unpack_from(f'{self.length}s', buffer, self.offset)[0].split(b'\00')[0]
         return self._decode_text(data)
 
     def build(self, text: str, buffer: bytearray) -> bytearray:
-        _buffer = bytearray(self.length)
         data = self._encode_text(text)[:self.length - 1] + b'\00'
-        length = len(data)
-        pack_into(f'{length}s', _buffer, self.offset, data)
-        buffer[:] = _buffer
+        if self.dummy:
+            _buffer = copy.deepcopy(self.dummy)
+        else:
+            _buffer = bytearray(self.length)
+        _buffer[0: len(data)] = data
+        pack_into(f'{self.length}s', buffer, self.offset, _buffer)
         return buffer
 
     def _decode_text(self, data: bytes) -> str:
@@ -28,7 +32,6 @@ class Text:
         if self.extra:
             for old, new in self.extra.items():
                 temp = temp.replace(old, new)
-        print(temp)
         return temp
 
     def _encode_text(self, text: str) -> bytearray:
