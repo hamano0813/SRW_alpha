8f77f3859266ae2d9c7e4b7f52d5125771ee3069
HamanoTaiki <hamano0813@gmail.com>
2022-05-26 15:38:19 +0800

    #152 更新COMMAND释义的简介和描述

--- src/widget/command/command_dialog.py
+++ src/widget/command/command_dialog.py
@@ -2,11 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QDialog
+from PySide6.QtWidgets import QDialog, QFormLayout
+from widget.command.command_explain import CommandExplain
 
 
 class CommandDialog(QDialog):
-    def __init__(self, parent, command: dict = None):
+    def __init__(self, parent, command: dict = None, **kwargs):
         super(CommandDialog, self).__init__(parent, f=Qt.Dialog)
         self.command = command if command else {'Pos': 0, 'Code': 0, 'Count': 1, 'Param': list(), 'Data': ''}
+        self.explain = CommandExplain(**kwargs)
+        self.widgets = list()
 
+    def get_command(self):
+        self.init_edit()
+
+    def init_edit(self):
+        self.widgets = list()
+        code = self.command['Code']
+
+        edit_layout = QFormLayout()
+        edit_layout.addRow('指令', )
