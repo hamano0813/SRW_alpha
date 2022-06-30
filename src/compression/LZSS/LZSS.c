1677ab9c418e06592d8e313674954d1786787580
Hamano0813 <hamano0813@gmail.com>
2022-06-30 09:14:05 +0800

    #193 调整解压程序方案为pyd方案

---
+++ src/compression/LZSS/LZSS.c
@@ -0,0 +1,139 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#define SIZEOFF 0
+#define DATAOFF 8
+#define MAXEXPAND 0x12
+
+int32_t ReadInt(uint8_t *Buffer, int32_t Offset) //读取无符号整数
+{
+	uint32_t Value;
+	Value = Buffer[Offset++];
+	Value += Buffer[Offset++] << 8;
+	Value += Buffer[Offset++] << 16;
+	Value += Buffer[Offset] << 24;
+	return Value;
+}
+
+void WriteInt(uint8_t *Buffer, int32_t Offset, int32_t Value) //写入无符号整数
+{
+	Buffer[Offset++] = Value & 0xFF;
+	Buffer[Offset++] = Value >> 8 & 0xFF;
+	Buffer[Offset++] = Value >> 16 & 0xFF;
+	Buffer[Offset] = Value >> 24 & 0xFF;
+}
+
+static PyObject *LZSS_compress(PyObject *self, PyObject *args)
+{
+	PyObject *InByteArray;
+	if (!PyArg_ParseTuple(args, "Y", &InByteArray))
+		return NULL;
+
+	uint8_t *InBuf;
+	InBuf = PyByteArray_AsString(InByteArray);
+	if (!InBuf)
+		return NULL;
+
+	int32_t InSize = (uint32_t)PyByteArray_Size(InByteArray);
+	int32_t MaxSize = InSize + (uint32_t)ceil(InSize / 8.0) + (InSize % 8);
+
+	uint8_t *OuBuf;
+	OuBuf = (uint8_t *)calloc(MaxSize, sizeof(uint8_t));
+	if (!OuBuf)
+		return PyErr_NoMemory();
+
+	WriteInt(OuBuf, SIZEOFF, InSize);
+
+	PyObject *OuByteArray = PyByteArray_FromStringAndSize(OuBuf, MaxSize);
+	free(OuBuf);
+	return OuByteArray;
+}
+
+static PyObject *LZSS_decompress(PyObject *self, PyObject *args)
+{
+	PyObject *InByteArray;
+	if (!PyArg_ParseTuple(args, "Y", &InByteArray))
+		return NULL;
+
+	uint8_t *InBuf;
+	InBuf = PyByteArray_AsString(InByteArray);
+	if (!InBuf)
+		return NULL;
+
+	int32_t InSize = (uint32_t)PyByteArray_Size(InByteArray);
+	int32_t OuSize = ReadInt(InBuf, SIZEOFF);
+
+	uint8_t *OuBuf;
+	OuBuf = (uint8_t *)calloc(OuSize, sizeof(uint8_t));
+	if (!OuBuf)
+		return PyErr_NoMemory();
+
+	int32_t RdCur = DATAOFF;
+	int32_t WrCur = 0;
+
+	int8_t ZipCnt = 0;
+	int16_t ZipIdx = 0;
+
+	while (RdCur < InSize)
+	{
+		uint8_t Flags = InBuf[RdCur++];
+		for (int flag_bit = 0; flag_bit < 8; flag_bit++)
+		{
+			if (Flags & (1 << flag_bit))
+				OuBuf[WrCur++] = InBuf[RdCur++];
+			else
+			{
+				ZipCnt = (InBuf[RdCur + 1] & 0xF) + MAXEXPAND - 0xF;
+				ZipIdx = (InBuf[RdCur + 1] & 0xF0) * 0x10 + InBuf[RdCur] + MAXEXPAND & 0xFFF;
+				if (ZipIdx > WrCur)
+					ZipIdx -= 0x1000;
+
+				for (int zip_idx = 0; zip_idx < ZipCnt; ++zip_idx)
+				{
+					if (ZipIdx >= 0)
+						OuBuf[WrCur++] = OuBuf[ZipIdx++];
+					else
+					{
+						OuBuf[WrCur++] = 0;
+						ZipIdx++;
+					}
+				}
+				RdCur += 2;
+			}
+			if (WrCur >= OuSize)
+				break;
+		}
+	}
+
+	PyObject *OuByteArray = PyByteArray_FromStringAndSize(OuBuf, OuSize);
+	free(OuBuf);
+	return OuByteArray;
+}
+
+static PyMethodDef LZSSMethods[] =
+	{
+		{"compress", LZSS_compress, METH_VARARGS, NULL},
+		{"decompress", LZSS_decompress, METH_VARARGS, NULL},
+		{NULL, NULL, 0, NULL} // Sentinel
+};
+
+static struct PyModuleDef LZSS_module =
+	{
+		PyModuleDef_HEAD_INIT,
+		"LZSS", // Module name
+		NULL,	// Documentation
+		-1,		// Size of per-interpreter state of the module, or -1 if the module keeps state in global variables.
+		LZSSMethods};
+
+PyMODINIT_FUNC PyInit_LZSS(void)
+{
+	PyObject *module;
+
+	module = PyModule_Create(&LZSS_module);
+	if (module == NULL)
+	{
+		return NULL;
+	}
+
+	return module;
+}
