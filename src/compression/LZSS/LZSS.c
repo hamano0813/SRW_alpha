d4c79920118766f32990a960aa58ba4d52579f00
Hamano0813 <hamano0813@gmail.com>
2022-07-01 09:01:40 +0800

    #196 更新解压算法

--- src/compression/LZSS/LZSS.c
+++ src/compression/LZSS/LZSS.c
@@ -3,71 +3,62 @@
 
 #define MAXEXPAND 0x12
 
-int32_t ReadInt(uint8_t *Buffer, int32_t Offset)
-{
-	uint32_t Value;
-	Value = Buffer[Offset++];
-	Value += Buffer[Offset++] << 8;
-	Value += Buffer[Offset++] << 16;
-	Value += Buffer[Offset] << 24;
-	return Value;
-}
-
-void WriteInt(uint8_t *Buffer, int32_t Offset, int32_t Value)
-{
-	Buffer[Offset++] = Value & 0xFF;
-	Buffer[Offset++] = Value >> 8 & 0xFF;
-	Buffer[Offset++] = Value >> 16 & 0xFF;
-	Buffer[Offset] = Value >> 24 & 0xFF;
-}
-
 static PyObject *LZSS_compress(PyObject *self, PyObject *args)
 {
+    // 读取待压缩的bytearray
 	PyObject *InByteArray;
 	if (!PyArg_ParseTuple(args, "Y", &InByteArray))
 		return NULL;
 
+    // 将输入bytearray转换成字节数组
 	uint8_t *InBuf;
 	InBuf = PyByteArray_AsString(InByteArray);
 	if (!InBuf)
 		return NULL;
 
+    // 计算输入bytearray大小并按可能的最大空间需求申请输出bytearray内存空间
 	int32_t InSize = (uint32_t)PyByteArray_Size(InByteArray);
 	int32_t MaxSize = InSize + (uint32_t)ceil(InSize / 8.0) + (InSize % 8);
-
 	uint8_t *OuBuf;
 	OuBuf = (uint8_t *)calloc(MaxSize, sizeof(uint8_t));
 	if (!OuBuf)
 		return PyErr_NoMemory();
 
+    // 将压缩完毕字节数组转为bytearray并释放申请的内存空间
 	PyObject *OuByteArray = PyByteArray_FromStringAndSize(OuBuf, MaxSize);
 	free(OuBuf);
+
 	return OuByteArray;
 }
 
 static PyObject *LZSS_decompress(PyObject *self, PyObject *args)
 {
+    // 读取待解压bytearray和输出bytearray大小
 	PyObject *InByteArray;
 	uint32_t OuSize;
 	if (!PyArg_ParseTuple(args, "YI", &InByteArray, &OuSize))
 		return NULL;
 
+    // 将输入bytearray转换成字节数组
 	uint8_t *InBuf;
 	InBuf = PyByteArray_AsString(InByteArray);
 	if (!InBuf)
 		return NULL;
 
+    // 计算输入bytearray大小并申请输出bytearray内存空间
+    uint32_t InSize = (uint32_t)PyByteArray_Size(InByteArray);
 	uint8_t *OuBuf;
 	OuBuf = (uint8_t *)calloc(OuSize, sizeof(uint8_t));
 	if (!OuBuf)
 		return PyErr_NoMemory();
 
-	uint32_t InSize = (uint32_t)PyByteArray_Size(InByteArray);
+    // 初始化解压所需参数
 	uint32_t RdCur = 0;
 	uint32_t WrCur = 0;
-	uint8_t ZipCnt = 0;
-	int16_t ZipIdx = 0;
+	uint8_t ZipCnt;
+	int16_t ZipIdx;
 
+    // 开始解压
 	while (RdCur < InSize)
 	{
 		uint8_t Flags = InBuf[RdCur++];
@@ -85,12 +76,10 @@ static PyObject *LZSS_decompress(PyObject *self, PyObject *args)
 				for (int zip_idx = 0; zip_idx < ZipCnt; ++zip_idx)
 				{
 					if (ZipIdx >= 0)
-						OuBuf[WrCur++] = OuBuf[ZipIdx++];
+						OuBuf[WrCur++] = OuBuf[ZipIdx];
 					else
-					{
 						OuBuf[WrCur++] = 0;
-						ZipIdx++;
-					}
+					ZipIdx++;
 				}
 				RdCur += 2;
 			}
@@ -99,35 +88,33 @@ static PyObject *LZSS_decompress(PyObject *self, PyObject *args)
 		}
 	}
 
+    // 将解压完毕字节数组转为bytearray并释放申请的内存空间
 	PyObject *OuByteArray = PyByteArray_FromStringAndSize(OuBuf, OuSize);
 	free(OuBuf);
+
 	return OuByteArray;
 }
 
 static PyMethodDef LZSSMethods[] =
 	{
-		{"compress", LZSS_compress, METH_VARARGS, NULL},
-		{"decompress", LZSS_decompress, METH_VARARGS, NULL},
-		{NULL, NULL, 0, NULL} // Sentinel
+		{"compress", LZSS_compress, METH_VARARGS, "compress data"},
+		{"decompress", LZSS_decompress, METH_VARARGS, "decompress data"},
+		{NULL, NULL, 0, NULL}
 };
 
 static struct PyModuleDef LZSS_module =
 	{
 		PyModuleDef_HEAD_INIT,
-		"LZSS", // Module name
-		NULL,	// Documentation
-		-1,		// Size of per-interpreter state of the module, or -1 if the module keeps state in global variables.
+		"LZSS",
+		"スパロボ大戦α LZSS-like Comprssion",
+		-1,
 		LZSSMethods};
 
 PyMODINIT_FUNC PyInit_LZSS(void)
 {
 	PyObject *module;
-
 	module = PyModule_Create(&LZSS_module);
 	if (module == NULL)
-	{
 		return NULL;
-	}
-
 	return module;
 }
