4473e092d48f305d7734f95e4490471ae27c5204
Hamano0813 <hamano0813@gmail.com>
2022-07-02 13:56:10 +0800

    #198 更新压缩解压算法为可用版本

--- src/compression/LZSS/LZSS.c
+++ src/compression/LZSS/LZSS.c
@@ -1,7 +1,17 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-#define MAXMATCH 0x12
+#define FLAGWIDTH 8
+#define THRESHOLD 2
+
+uint8_t GetPreBits(uint8_t *Buffer, uint32_t Offset)
+{
+	return Buffer[Offset + 1] & (1 << 4) - 1;
+}
+uint16_t GetFwdBits(uint8_t *Buffer, uint32_t Offset)
+{
+	return (Buffer[Offset + 1] >> 4 << 8) | Buffer[Offset];
+}
 
 static PyObject *LZSS_compress(PyObject *self, PyObject *args)
 {
@@ -11,160 +21,155 @@ static PyObject *LZSS_compress(PyObject *self, PyObject *args)
 		return NULL;
 
 	// 将输入bytearray转换成字节数组
-	uint8_t *InBuf;
-	InBuf = PyByteArray_AsString(InByteArray);
-	if (!InBuf)
+	uint8_t *InBuffer;
+	InBuffer = PyByteArray_AsString(InByteArray);
+	if (!InBuffer)
 		return NULL;
 
 	// 计算输入bytearray大小并按可能的最大空间需求申请输出bytearray内存空间
 	uint32_t InSize = (uint32_t)PyByteArray_Size(InByteArray);
 	uint32_t MaxSize = InSize + (uint32_t)ceil(InSize / 8.0) + (InSize % 8);
-	uint8_t *OuBuf;
-	OuBuf = (uint8_t *)calloc(MaxSize, sizeof(uint8_t));
-	if (!OuBuf)
+	uint8_t *OutBuffer;
+	OutBuffer = (uint8_t *)calloc(MaxSize, sizeof(uint8_t));
+	if (!OutBuffer)
 		return PyErr_NoMemory();
 
-	uint32_t MVal;
-	uint32_t FlagsCur;
-	uint32_t WrCur = 0;
-	uint32_t BlkCnt = 8;
+	uint32_t MatchValue;
+	uint32_t FlagsOffset;
+	uint32_t WriteOffset = 0;
+	uint32_t FlagBit = 8;
 
-	for (uint32_t RdCur = 0; RdCur < InSize; RdCur++)
+	for (int ReadOffset = 0; ReadOffset < (int)InSize; ReadOffset++)
 	{
-		if (BlkCnt == 8)
+		if (FlagBit == 8)
 		{
-			FlagsCur = WrCur++;
-			OuBuf[FlagsCur] = 0;
-			BlkCnt = 0;
+			FlagsOffset = WriteOffset++;
+			OutBuffer[FlagsOffset] = 0;
+			FlagBit = 0;
 		}
-		int num2 = (((RdCur + 0xfee) / 0x1000) * 0x1000) - 0xfee;
-		int ZipCnt = 0;
-		int ZipIdx = 0;
-		for (int m_cur = RdCur - 1; m_cur > ((int32_t)RdCur - 0x1000); m_cur--)
+		int16_t ReadFlag = (((ReadOffset + 0xfee) / 0x1000) * 0x1000) - 0xfee;
+		int8_t MatchCount = 0;
+		int16_t MatchOffset = 0;
+		for (int PreIndex = ReadOffset - 1; PreIndex >= (ReadOffset - 0xFFF); PreIndex--)
 		{
-			int MCtn = 0;
-			while (MCtn < MAXMATCH)
+			int FwdIndex = 0;
+			while (FwdIndex <= 0x11)
 			{
-				MVal = 0;
-				if ((m_cur + MCtn) >= 0)
+				MatchValue = 0;
+				if ((PreIndex + FwdIndex) >= 0)
+					MatchValue = InBuffer[PreIndex + FwdIndex];
+				if ((ReadOffset + FwdIndex) == InSize)
+					break;
+				if (InBuffer[ReadOffset + FwdIndex] != MatchValue)
+					break;
+				FwdIndex++;
+			}
+
+			if (FwdIndex > MatchCount)
+			{
+				MatchOffset = PreIndex - ReadFlag;
+
+				if (MatchOffset < 0)
 				{
-					MVal = InBuf[m_cur + MCtn];
+					MatchOffset += 0x1000;
 				}
-				if (((RdCur + MCtn) == InSize) || (InBuf[RdCur + MCtn] != MVal))
+				MatchCount = FwdIndex;
+				if (MatchCount == 0x12)
 				{
 					break;
 				}
-				MCtn++;
 			}
-			if (MCtn > ZipCnt)
-			{
-				ZipIdx = m_cur - num2;
-				if (ZipIdx < 0)
-				{
-					ZipIdx += 0x1000;
-				}
-				ZipCnt = MCtn;
-				if (ZipCnt == 0x12)
-				{
-					break;
-				}
-			}
-			if ((RdCur + MCtn) == InSize)
+			if ((ReadOffset + FwdIndex) == InSize)
 			{
 				break;
 			}
 		}
-		OuBuf[FlagsCur] = (int8_t)(OuBuf[FlagsCur] / 2);
-		BlkCnt++;
-		if (ZipCnt > 2)
+
+		OutBuffer[FlagsOffset] = (int8_t)(OutBuffer[FlagsOffset] / 2);
+		FlagBit++;
+		if (MatchCount > 2)
 		{
-			OuBuf[WrCur] = (int8_t)(ZipIdx % 0x100);
-			WrCur++;
-			OuBuf[WrCur] = (int8_t)((((ZipIdx / 0x100) * 0x10) + ZipCnt) - 3);
-			WrCur++;
-			RdCur = (RdCur + ZipCnt) - 1;
+			// printf("%X\t%X\t%X\n", MatchOffset, MatchCount, num2);
+			OutBuffer[WriteOffset] = (uint8_t)(MatchOffset % 0x100);
+			WriteOffset++;
+			OutBuffer[WriteOffset] = (uint8_t)((((MatchOffset / 0x100) * 0x10) + MatchCount) - 3);
+			WriteOffset++;
+			ReadOffset = (ReadOffset + MatchCount) - 1;
 		}
 		else
 		{
-			OuBuf[FlagsCur] = (int8_t)(OuBuf[FlagsCur] + 0x80);
-			OuBuf[WrCur] = InBuf[RdCur];
-			WrCur++;
+			OutBuffer[FlagsOffset] = (int8_t)(OutBuffer[FlagsOffset] + 0x80);
+			OutBuffer[WriteOffset] = InBuffer[ReadOffset];
+			WriteOffset++;
 		}
 	}
-	for (MVal = 1; MVal <= (8 - BlkCnt); MVal++)
+	for (MatchValue = 1; MatchValue <= (8 - FlagBit); MatchValue++)
 	{
-		OuBuf[FlagsCur] = (int8_t)(OuBuf[FlagsCur] / 2);
+		OutBuffer[FlagsOffset] = (int8_t)(OutBuffer[FlagsOffset] / 2);
 	}
 
 	// 将压缩完毕字节数组转为bytearray并释放申请的内存空间
-	PyObject *OuByteArray = PyByteArray_FromStringAndSize(OuBuf, WrCur);
-	free(OuBuf);
+	PyObject *OuByteArray = PyByteArray_FromStringAndSize(OutBuffer, WriteOffset);
+	free(OutBuffer);
 
 	return OuByteArray;
 }
 
 static PyObject *LZSS_decompress(PyObject *self, PyObject *args)
 {
-	// 读取待解压bytearray和输出bytearray大小
 	PyObject *InByteArray;
-	uint32_t OuSize;
-	if (!PyArg_ParseTuple(args, "YI", &InByteArray, &OuSize))
+	uint32_t OutSize;
+	if (!PyArg_ParseTuple(args, "YI", &InByteArray, &OutSize))
 		return NULL;
 
-	// 将输入bytearray转换成字节数组
-	uint8_t *InBuf;
-	InBuf = PyByteArray_AsString(InByteArray);
-	if (!InBuf)
+	uint8_t *InBuffer;
+	InBuffer = PyByteArray_AsString(InByteArray);
+	if (!InBuffer)
 		return NULL;
 
-	// 计算输入bytearray大小并申请输出bytearray内存空间
-	uint32_t InSize = (uint32_t)PyByteArray_Size(InByteArray);
-	uint8_t *OuBuf;
-	OuBuf = (uint8_t *)calloc(OuSize, sizeof(uint8_t));
-	if (!OuBuf)
+	uint8_t *OutBuffer;
+	OutBuffer = (uint8_t *)calloc(OutSize, sizeof(uint8_t));
+	if (!OutBuffer)
 		return PyErr_NoMemory();
 
-	// 初始化解压所需参数
-	uint32_t RdCur = 0;
-	uint32_t WrCur = 0;
-	uint8_t ZipCnt;
-	int16_t ZipIdx;
+	uint32_t ReadOffset = 0;
+	uint32_t WriteOffset = 0;
+	uint8_t MatchCount;
+	int16_t MatchOffset;
 
-	// 开始解压
-	while (RdCur < InSize)
+	while (WriteOffset < OutSize)
 	{
-		uint8_t Flags = InBuf[RdCur++];
-		for (int flag_bit = 0; flag_bit < 8; flag_bit++)
+		uint8_t Flags = InBuffer[ReadOffset++];
+		for (int FlagBit = 0; FlagBit < FLAGWIDTH; FlagBit++)
 		{
-			if (Flags & (1 << flag_bit))
-				OuBuf[WrCur++] = InBuf[RdCur++];
+			if (Flags & (1 << FlagBit))
+				OutBuffer[WriteOffset++] = InBuffer[ReadOffset++];
 			else
 			{
-				ZipCnt = (InBuf[RdCur + 1] & 0xF) + MAXMATCH - 0xF;
-				ZipIdx = (InBuf[RdCur + 1] & 0xF0) * 0x10 + InBuf[RdCur] + MAXMATCH & 0xFFF;
-				if ((uint16_t)ZipIdx > WrCur)
-					ZipIdx -= 0x1000;
+				MatchCount = GetPreBits(InBuffer, ReadOffset) + THRESHOLD + 1;
+				MatchOffset = GetFwdBits(InBuffer, ReadOffset) + THRESHOLD + 0x10 & 0xFFF;
 
-				for (int zip_idx = 0; zip_idx < ZipCnt; ++zip_idx)
+				if ((uint16_t)MatchOffset > WriteOffset)
+					MatchOffset -= 0x1000;
+
+				for (int MatchIndex = 0; MatchIndex < MatchCount; MatchIndex++, MatchOffset++)
 				{
-					if (ZipIdx < 0)
-						OuBuf[WrCur++] = 0;
+					if (MatchOffset < 0)
+						OutBuffer[WriteOffset++] = 0;
 					else
-						OuBuf[WrCur++] = OuBuf[ZipIdx];
-					ZipIdx++;
+						OutBuffer[WriteOffset++] = OutBuffer[MatchOffset];
 				}
-				RdCur += 2;
+				ReadOffset += 2;
 			}
-			if (WrCur >= OuSize)
+			if (WriteOffset >= OutSize)
 				break;
 		}
 	}
 
-	// 将解压完毕字节数组转为bytearray并释放申请的内存空间
-	PyObject *OuByteArray = PyByteArray_FromStringAndSize(OuBuf, OuSize);
-	free(OuBuf);
+	PyObject *OutByteArray = PyByteArray_FromStringAndSize(OutBuffer, OutSize);
+	free(OutBuffer);
 
-	return OuByteArray;
+	return OutByteArray;
 }
 
 static PyMethodDef LZSSMethods[] =
