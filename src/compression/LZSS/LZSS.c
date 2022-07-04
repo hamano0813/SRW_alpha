46be1574fa40bbff2145c7619905979fe7b2b6c9
Hamano0813 <hamano0813@gmail.com>
2022-07-04 08:55:09 +0800

    #199 更新压缩解压算法的改良

--- src/compression/LZSS/LZSS.c
+++ src/compression/LZSS/LZSS.c
@@ -1,116 +1,109 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-#define FLAGWIDTH 8
-#define THRESHOLD 2
+#define BYTE unsigned char
+#define WORD short
+#define DWORD int
 
-uint8_t GetPreBits(uint8_t *Buffer, uint32_t Offset)
-{
-	return Buffer[Offset + 1] & (1 << 4) - 1;
-}
-uint16_t GetFwdBits(uint8_t *Buffer, uint32_t Offset)
-{
-	return (Buffer[Offset + 1] >> 4 << 8) | Buffer[Offset];
-}
+#define MAXSIZE 0x8000
+#define THRESHOLD 2
+#define PREBUFBIT 4
+#define WINBUFBIT 12
 
 static PyObject *LZSS_compress(PyObject *self, PyObject *args)
 {
-	// 读取待压缩的bytearray
 	PyObject *InByteArray;
 	if (!PyArg_ParseTuple(args, "Y", &InByteArray))
 		return NULL;
-
-	// 将输入bytearray转换成字节数组
-	uint8_t *InBuffer;
+	BYTE *InBuffer;
 	InBuffer = PyByteArray_AsString(InByteArray);
 	if (!InBuffer)
 		return NULL;
-
-	// 计算输入bytearray大小并按可能的最大空间需求申请输出bytearray内存空间
-	uint32_t InSize = (uint32_t)PyByteArray_Size(InByteArray);
-	uint32_t MaxSize = InSize + (uint32_t)ceil(InSize / 8.0) + (InSize % 8);
-	uint8_t *OutBuffer;
-	OutBuffer = (uint8_t *)calloc(MaxSize, sizeof(uint8_t));
-	if (!OutBuffer)
+	BYTE *OutBuf;
+	OutBuf = (BYTE *)calloc(MAXSIZE, sizeof(BYTE));
+	if (!OutBuf)
 		return PyErr_NoMemory();
 
-	uint32_t MatchValue;
-	uint32_t FlagsOffset;
-	uint32_t WriteOffset = 0;
-	uint32_t FlagBit = 8;
+	const BYTE PreSize = (1 << PREBUFBIT);
+	const WORD WinSize = (1 << WINBUFBIT);
+	const DWORD RdSize = (DWORD)PyByteArray_Size(InByteArray);
 
-	for (int ReadOffset = 0; ReadOffset < (int)InSize; ReadOffset++)
+	DWORD FlagCur;
+	DWORD WrCur = 0;
+	BYTE FlagBit = 8;
+	BYTE MatchValue;
+
+	for (DWORD RdCur = 0; RdCur < RdSize; RdCur++)
 	{
 		if (FlagBit == 8)
 		{
-			FlagsOffset = WriteOffset++;
-			OutBuffer[FlagsOffset] = 0;
+			FlagCur = WrCur++;
+			OutBuf[FlagCur] = 0;
 			FlagBit = 0;
 		}
-		int16_t ReadFlag = (((ReadOffset + 0xfee) / 0x1000) * 0x1000) - 0xfee;
-		int8_t MatchCount = 0;
-		int16_t MatchOffset = 0;
-		for (int PreIndex = ReadOffset - 1; PreIndex >= (ReadOffset - 0xFFF); PreIndex--)
+		WORD OffsetAdjust = (((RdCur + 0xFEE) / WinSize) * WinSize) - 0xFEE;
+		BYTE MatchCount = 0;
+		WORD MatchOffset = 0;
+		for (DWORD PreIndex = RdCur - 1; PreIndex > (RdCur - WinSize); PreIndex--)
 		{
-			int FwdIndex = 0;
-			while (FwdIndex <= 0x11)
+			BYTE match_idx =0;
+			for (match_idx; match_idx < (PreSize + 2); match_idx++)
 			{
 				MatchValue = 0;
-				if ((PreIndex + FwdIndex) >= 0)
-					MatchValue = InBuffer[PreIndex + FwdIndex];
-				if ((ReadOffset + FwdIndex) == InSize)
+				if ((PreIndex + match_idx) >= 0)
+					MatchValue = InBuffer[PreIndex + match_idx];
+				if ((RdCur + match_idx) == RdSize)
 					break;
-				if (InBuffer[ReadOffset + FwdIndex] != MatchValue)
+				if (InBuffer[RdCur + match_idx] != MatchValue)
 					break;
-				FwdIndex++;
 			}
 
-			if (FwdIndex > MatchCount)
+			if (match_idx > MatchCount)
 			{
-				MatchOffset = PreIndex - ReadFlag;
+				MatchOffset = PreIndex - OffsetAdjust;
 
 				if (MatchOffset < 0)
 				{
-					MatchOffset += 0x1000;
+					MatchOffset += WinSize;
 				}
-				MatchCount = FwdIndex;
+				MatchCount = match_idx;
 				if (MatchCount == 0x12)
 				{
 					break;
 				}
 			}
-			if ((ReadOffset + FwdIndex) == InSize)
+			if ((RdCur + match_idx) == RdSize)
 			{
 				break;
 			}
 		}
 
-		OutBuffer[FlagsOffset] = (int8_t)(OutBuffer[FlagsOffset] / 2);
+		OutBuf[FlagCur] = (int8_t)(OutBuf[FlagCur] / 2);
 		FlagBit++;
 		if (MatchCount > 2)
 		{
 			// printf("%X\t%X\t%X\n", MatchOffset, MatchCount, num2);
-			OutBuffer[WriteOffset] = (uint8_t)(MatchOffset % 0x100);
-			WriteOffset++;
-			OutBuffer[WriteOffset] = (uint8_t)((((MatchOffset / 0x100) * 0x10) + MatchCount) - 3);
-			WriteOffset++;
-			ReadOffset = (ReadOffset + MatchCount) - 1;
+			OutBuf[WrCur] = (uint8_t)(MatchOffset % 0x100);
+			WrCur++;
+			OutBuf[WrCur] = (uint8_t)((((MatchOffset / 0x100) * 0x10) + MatchCount) - 3);
+			WrCur++;
+			RdCur = (RdCur + MatchCount) - 1;
 		}
 		else
 		{
-			OutBuffer[FlagsOffset] = (int8_t)(OutBuffer[FlagsOffset] + 0x80);
-			OutBuffer[WriteOffset] = InBuffer[ReadOffset];
-			WriteOffset++;
+			OutBuf[FlagCur] = (int8_t)(OutBuf[FlagCur] + 0x80);
+			OutBuf[WrCur] = InBuffer[RdCur];
+			WrCur++;
 		}
 	}
 	for (MatchValue = 1; MatchValue <= (8 - FlagBit); MatchValue++)
 	{
-		OutBuffer[FlagsOffset] = (int8_t)(OutBuffer[FlagsOffset] / 2);
+		OutBuf[FlagCur] = (int8_t)(OutBuf[FlagCur] / 2);
 	}
 
 	// 将压缩完毕字节数组转为bytearray并释放申请的内存空间
-	PyObject *OuByteArray = PyByteArray_FromStringAndSize(OutBuffer, WriteOffset);
-	free(OutBuffer);
+	PyObject *OuByteArray = PyByteArray_FromStringAndSize(OutBuf, WrCur);
+	free(OutBuf);
 
 	return OuByteArray;
 }
@@ -118,71 +111,66 @@ static PyObject *LZSS_compress(PyObject *self, PyObject *args)
 static PyObject *LZSS_decompress(PyObject *self, PyObject *args)
 {
 	PyObject *InByteArray;
-	uint32_t OutSize;
-	if (!PyArg_ParseTuple(args, "YI", &InByteArray, &OutSize))
+	if (!PyArg_ParseTuple(args, "Y", &InByteArray))
 		return NULL;
-
-	uint8_t *InBuffer;
-	InBuffer = PyByteArray_AsString(InByteArray);
-	if (!InBuffer)
+	BYTE *InBuf;
+	InBuf = PyByteArray_AsString(InByteArray);
+	if (!InBuf)
 		return NULL;
-
-	uint8_t *OutBuffer;
-	OutBuffer = (uint8_t *)calloc(OutSize, sizeof(uint8_t));
-	if (!OutBuffer)
+	BYTE *OutBuf;
+	OutBuf = (BYTE *)calloc(MAXSIZE, sizeof(BYTE));
+	if (!OutBuf)
 		return PyErr_NoMemory();
 
-	uint32_t ReadOffset = 0;
-	uint32_t WriteOffset = 0;
-	uint8_t MatchCount;
-	int16_t MatchOffset;
+	const BYTE PreSize = (1 << PREBUFBIT);
+	const WORD WinSize = (1 << WINBUFBIT);
+	const DWORD RdSize = (DWORD)PyByteArray_Size(InByteArray);
+	DWORD RdCur = 0;
+	DWORD WrCur = 0;
 
-	while (WriteOffset < OutSize)
+	while (RdCur < RdSize)
 	{
-		uint8_t Flags = InBuffer[ReadOffset++];
-		for (int FlagBit = 0; FlagBit < FLAGWIDTH; FlagBit++)
+		BYTE flagBits = InBuf[RdCur++];
+		for (BYTE bit_idx = 0; bit_idx < CHAR_BIT; bit_idx++)
 		{
-			if (Flags & (1 << FlagBit))
-				OutBuffer[WriteOffset++] = InBuffer[ReadOffset++];
+			if (flagBits & (1 << bit_idx))
+				OutBuf[WrCur++] = InBuf[RdCur++];
 			else
 			{
-				MatchCount = GetPreBits(InBuffer, ReadOffset) + THRESHOLD + 1;
-				MatchOffset = GetFwdBits(InBuffer, ReadOffset) + THRESHOLD + 0x10 & 0xFFF;
+				BYTE matchCnt = (InBuf[RdCur + 1] & (PreSize - 1)) + 1 + THRESHOLD;
+				WORD matchCur = (InBuf[RdCur + 1] >> PREBUFBIT << CHAR_BIT | InBuf[RdCur]) + PreSize + THRESHOLD & (WinSize - 1);
+				if (matchCur > WrCur)
+					matchCur -= WinSize;
 
-				if ((uint16_t)MatchOffset > WriteOffset)
-					MatchOffset -= 0x1000;
-
-				for (int MatchIndex = 0; MatchIndex < MatchCount; MatchIndex++, MatchOffset++)
+				for (int match_idx = 0; match_idx < matchCnt; match_idx++, matchCur++)
 				{
-					if (MatchOffset < 0)
-						OutBuffer[WriteOffset++] = 0;
+					if (matchCur < 0)
+						OutBuf[WrCur++] = 0;
 					else
-						OutBuffer[WriteOffset++] = OutBuffer[MatchOffset];
+						OutBuf[WrCur++] = OutBuf[matchCur];
 				}
-				ReadOffset += 2;
+				RdCur += 2;
 			}
-			if (WriteOffset >= OutSize)
+			if (RdCur >= RdSize)
 				break;
 		}
 	}
-
-	PyObject *OutByteArray = PyByteArray_FromStringAndSize(OutBuffer, OutSize);
-	free(OutBuffer);
-
+	PyObject *OutByteArray = PyByteArray_FromStringAndSize(OutBuf, WrCur);
+	free(OutBuf);
 	return OutByteArray;
 }
 
 static PyMethodDef LZSSMethods[] =
 	{
-		{"compress", LZSS_compress, METH_VARARGS, "compress data"},
-		{"decompress", LZSS_decompress, METH_VARARGS, "decompress data"},
+		{"compress", LZSS_compress, METH_VARARGS, "use the LZSS algorithm to compress data."},
+		{"decompress", LZSS_decompress, METH_VARARGS, "use the LZSS algorithm to decompress data."},
 		{NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef LZSS_module =
 	{
 		PyModuleDef_HEAD_INIT,
 		"LZSS",
-		"スパロボ大戦α LZSS-like Comprssion",
+		"special LZSS compression algorithm for スパロボット大戦α",
 		-1,
 		LZSSMethods};
 
--- src/test/lzss_test.py
+++ src/test/lzss_test.py
@@ -27,7 +27,7 @@ def test_decompress():
         buf = o_data[off + ptrs[u_idx]: off + ptrs[u_idx + 1]]
         ou_size = unpack_from('I', buf, 0)[0]
         # noinspection PyUnresolvedReferences
-        decom = LZSS.decompress(buf[8:], ou_size)
+        decom = LZSS.decompress(buf[8:])
         buffer += decom
         if not decom == s_data[0x2C4 * u_idx: 0x2C4 * (u_idx + 1)]:
             print(u_idx)
@@ -38,23 +38,17 @@ def test_compress():
     for u_idx in range(cnt):
         buf = o_data[off + ptrs[u_idx]: off + ptrs[u_idx + 1]]
         ou_size = unpack_from('I', buf, 0)[0]
-        decom = LZSS.decompress(buf[8:], ou_size)
+        decom = LZSS.decompress(buf[8:])
         com = LZSS.compress(decom)
-        # print(decom == LZSS.decompress(com, ou_size))
-        com = LZSS.compress(decom)
-        # with open('d:/a01.bin', 'wb') as f:
-        #     f.write(buf[8:])
-        # with open('d:/b01.bin', 'wb') as f:
-        #     f.write(com)
-        # print(decom == s_data[0x2C4*u_idx: 0x2C4*u_idx+0x2C4])
-        # print(decom == LZSS.decompress(com, ou_size))
-        # if u_idx == 0:
-        #     for i, d in enumerate(com):
-        #         print(i, f"{d:02X}", f"{buf[8 + i]:02X}", d == buf[8 + i], sep='\t')
+        print(buf[8:] == com, decom == LZSS.decompress(com))
+        # if u_idx == 31:
+        #     with open('d:/a031.bin', 'wb') as f:
+        #         f.write(LZSS.decompress(com, ou_size))
 
 
 start = time.time()
 test_compress()
+test_decompress()
 end = time.time()
 
 print(f"{end - start:.3f}")
