dee4c0bb9f6a22968e0af537d2f019d2dcb72baa
hamano0813 <hamano0813@gmail.com>
2022-07-13 15:51:47 +0800

    #210 更新COMMAND解析函数以方面C扩展

--- src/structure/specific/sndata_bin/sndata.py
+++ src/structure/specific/sndata_bin/sndata.py
@@ -3,7 +3,6 @@
 
 from structure.generic import Rom
 from structure.specific.sndata_bin.scenario import Scenario, SCENARIO_STRUCTURE
-from structure.destructor import SNDATA
 
 
 class SndataBIN(Rom):
@@ -12,18 +11,3 @@ class SndataBIN(Rom):
         self.structures = {
             '场景设计': Scenario(SCENARIO_STRUCTURE, 0x800, 0x4048, 0x8C),
         }
-
-    def parse(self) -> bool:
-        if not self.buffer:
-            return False
-        for pname, structure in self.structures.items():
-            self.data[pname] = structure.parse(self.buffer)
-        SNDATA.parse(self.buffer)
-        return True
-
-    def build(self) -> bool:
-        if not self.data:
-            return False
-        for pname, data in self.data.items():
-            self.structures[pname].build(data, self.buffer)
-        return True
