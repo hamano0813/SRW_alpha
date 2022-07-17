15aa875909b24dc025cf69c9b7c5f67684e96bbe
Hamano0813 <hamano0813@gmail.com>
2022-07-17 16:42:54 +0800

    #221 调整AIUNP的模块设计

--- src/structure/specific/aiunp_bin.py
+++ src/structure/specific/aiunp_bin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from structure.generic import Value, Sequence, SEQUENCE
+from structure.generic import Rom, Value, Sequence, SEQUENCE
 
 AI_STRUCTURE = {
     'AI': Value(0x0, 0x2),
@@ -75,3 +75,19 @@ class AiSetting(Sequence):
     @staticmethod
     def _calc_length(value: int, step: int = 0x4) -> int:
         return (value // step + bool(value % step)) * step
+
+
+class AiunpBIN(Rom):
+    def __init__(self):
+        super(AiunpBIN, self).__init__()
+        self.structures = {
+            '指针列表': Sequence({'指针': Value(0x0, 0x4)}, 0x0, 0x4, 0x8D),
+            'AI设计': AiSetting(SETTING_STRUCTURE, 0x800, 0x0, 0x8C),
+        }
+
+    def parse(self) -> bool:
+        if not self.buffer:
+            return False
+        self['指针列表'] = self.structures['AI设计'].pointers = self.structures['指针列表'].parse(self.buffer)
+        self['AI设计'] = self.structures['AI设计'].parse(self.buffer)
+        return True
--- src/structure/specific/aiunp_bin/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from structure.specific.aiunp_bin.ai import AI_STRUCTURE
-from structure.specific.aiunp_bin.aiunp import AiunpBIN
