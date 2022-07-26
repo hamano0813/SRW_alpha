5e35d7d0fa5272fc1a69e4ad1517d8e909e6e694
Hamano0813 <hamano0813@gmail.com>
2022-07-26 22:53:05 +0800

    #234 更新LZSS算法的测试单元

--- src/test/lzss_test.py
+++ src/test/lzss_test.py
@@ -15,33 +15,34 @@ def diff_print(l_buf: bytes | bytearray, r_buf: bytes | bytearray = None):
     for i_idx in range(max(li_size := len(l_buf), ri_size := len(r_buf))):
         if i_idx < li_size and i_idx < ri_size:
             if l_buf[i_idx] == r_buf[i_idx]:
-                li_list.append(f" {l_buf[i_idx]:02X}")
-                ri_list.append(f" {r_buf[i_idx]:02X}")
+                li_list.append(f"{l_buf[i_idx]:02X}")
+                ri_list.append(f"{r_buf[i_idx]:02X}")
             else:
-                li_list.append(f" \033[1;39;47m{l_buf[i_idx]:02X}\033[0m")
-                ri_list.append(f" \033[1;39;47m{r_buf[i_idx]:02X}\033[0m")
+                li_list.append(f"\033[1;39;47m{l_buf[i_idx]:02X}\033[0m")
+                ri_list.append(f"\033[1;39;47m{r_buf[i_idx]:02X}\033[0m")
         elif i_idx >= li_size:
-            li_list.append(f"   ")
-            ri_list.append(f" \033[4m{r_buf[i_idx]:02X}\033[0m")
+            li_list.append(f"  ")
+            ri_list.append(f"\033[4m{r_buf[i_idx]:02X}\033[0m")
         elif i_idx >= ri_size:
-            li_list.append(f" \033[4m{l_buf[i_idx]:02X}\033[0m")
-            ri_list.append(f"   ")
+            li_list.append(f"\033[4m{l_buf[i_idx]:02X}\033[0m")
+            ri_list.append(f"  ")
+    # noinspection PyUnboundLocalVariable
     for p_idx in range(0x10 - i_idx % 0x10 - 1):
-        li_list.append(f"   ")
-        ri_list.append(f"   ")
+        li_list.append(f"  ")
+        ri_list.append(f"  ")
 
     lt_list = [
-        f"\033[7m {i:>08X}h: \033[0m " + " ".join(li_list[i: i + 0x10])
+        f"\033[7m{i:>08X}h:\033[0m " + " ".join(li_list[i: i + 0x10])
         for i in range(0, len(li_list), 0x10)
     ]
     rt_list = [
-        f"\033[7m {i:>08X}h: \033[0m " + " ".join(ri_list[i: i + 0x10])
+        f"\033[7m{i:>08X}h:\033[0m " + " ".join(ri_list[i: i + 0x10])
         for i in range(0, len(ri_list), 0x10)
     ]
     size = max(lt_size := len(lt_list), rt_size := len(rt_list))
 
-    header = " ".join([f"\033[7m{h_idx:^3X}\033[0m" for h_idx in range(0x10)])
-    print(" " * 13 + header + "\t" + " " * 13 + header)
+    header = " " * 11 + " ".join([f"\033[7m{s}\033[0m" for s in "０１２３４５６７８９ＡＢＣＤＥＦ"])
+    print(header + "\t" + header)
     for t_idx in range(size):
         if t_idx >= lt_size:
             print(" " * 64 + f"{rt_list[t_idx]}")
@@ -84,45 +85,41 @@ def lzss_print(buf: bytes | bytearray):
     print(*text, sep="\n")
 
 
-o_path = r'..\..\resource\bin\jp\ROBOT.RAF'
-s_path = r'..\..\resource\bin\jp\UNCOMPRESS_ROBOT.RAF'
+def test_raf():
+    path = r'..\..\resource\bin\jp\ROBOT.RAF'
+    with open(path, 'rb') as f:
+        data = bytearray(f.read())
+    cnt = unpack_from('I', data, 0x0)[0]
+    off = (cnt + 1) * 4
+    ptrs = list(unpack_from('I' * cnt, data, 0x4))
+    ptrs.append(len(data))
 
-with open(o_path, 'rb') as o_file:
-    o_data = bytearray(o_file.read())
-
-with open(s_path, 'rb') as s_file:
-    s_data = bytearray(s_file.read())
-
-cnt = unpack_from('I', o_data, 0x0)[0]
-off = (cnt + 1) * 4
-ptrs = list(unpack_from('I' * cnt, o_data, 0x4))
-ptrs.append(len(o_data))
-
-
-def test_decompress():
-    buffer = bytearray()
     for u_idx in range(cnt):
-        buf = o_data[off + ptrs[u_idx]: off + ptrs[u_idx + 1]]
-        # noinspection PyUnresolvedReferences
-        decom = LZSS.decompress(buf)
-        buffer += decom
-        if not decom == s_data[0x2C4 * u_idx: 0x2C4 * (u_idx + 1)]:
-            print(u_idx)
-    return buffer
+        o_buf = data[off + ptrs[u_idx]: off + ptrs[u_idx + 1]]
+        ms_buf = LZSS.decompress(o_buf)
+        mo_buf = LZSS.compress(ms_buf)
+        s_buf = LZSS.decompress(mo_buf)
+        print(f"{u_idx:03X}", "\tdecompress", ms_buf == s_buf, "\tcompress", mo_buf == o_buf, f"\t{len(ms_buf):>8X}")
 
 
-def test_compress():
+def test_bin():
+    path = r'..\..\resource\bin\jp\DR.BIN'
+    with open(path, 'rb') as f:
+        data = bytearray(f.read())
+    cnt = unpack_from('I', data, 0x0)[0] // 4 - 1
+    ptrs = list(unpack_from('I' * (cnt + 1), data, 0x0))
+
     for u_idx in range(cnt):
-        s_buf = s_data[0x2C4 * u_idx: 0x2C4 * (u_idx + 1)]
-        com = LZSS.compress(s_buf)
-        # print(u_idx, com == o_data[off + ptrs[u_idx]: off + ptrs[u_idx + 1]])
-        # buf = o_data[off + ptrs[u_idx]: off + ptrs[u_idx + 1]]
-        # print(u_idx, buf == com, LZSS.decompress(com) == s_data[0x2C4 * u_idx: 0x2C4 * (u_idx + 1)])
+        o_buf = data[ptrs[u_idx]: ptrs[u_idx + 1]]
+        ms_buf = LZSS.decompress(o_buf)
+        mo_buf = LZSS.compress(ms_buf, 4)
+        s_buf = LZSS.decompress(mo_buf)
+        print(f"{u_idx:03X}", "\tdecompress", ms_buf == s_buf, "\tcompress", mo_buf == o_buf, f"\t{len(ms_buf):>8X}")
 
 
 start = time.time()
-# test_compress()
-test_decompress()
+test_raf()
+# test_bin()
 end = time.time()
 
 print(f"{end - start:.3f}")
